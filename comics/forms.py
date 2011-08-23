19dcf1e9319d87cbbbfcb3467b6e2dfd2c6ab42f
Joe Friedl <joe@joefriedl.net>
2011-08-22 22:01:10 -0400

    More contact stuff

--- comics/forms.py
+++ comics/forms.py
@@ -4,6 +4,5 @@ class ContactForm(forms.Form):
     name = forms.CharField(max_length=120,
             widget=forms.TextInput(attrs={'size':32}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'size':32}))
-    website = forms.URLField()
     message = forms.CharField(
             widget=forms.Textarea(attrs={'rows':8, 'cols':32}))
