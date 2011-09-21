c9d1dee07c3569e76a3cca456b1b4e149396d583
Joe Friedl <joe@joefriedl.net>
2011-09-21 16:01:13 -0400

    Changin settins

--- live_settings.py
+++ live_settings.py
@@ -49,7 +49,7 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/var/www/media/zombiewalrus/'
+MEDIA_ROOT = '/var/www/django/zombiewalrus/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
