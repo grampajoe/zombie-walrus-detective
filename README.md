664fcc6963ee3c5b694a54da9cc74782d63b9d27
Joe Friedl <jfriedl@digitalocean.com>
2017-05-30 16:46:19 -0400

    Cache the image tag to avoid hitting S3 so much

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # zombie-walrus-detective
+
 Zombie Walrus Detective!!!
+
+## Running with Docker
+
+To run this thing with Docker, you'll need `docker` and `docker-compose`.
+
+Build the images:
+```bash
+$ docker-compose build
+```
+
+Create the database and a superuser:
+```bash
+$ docker-compose run app python manage.py syncdb
+```
+
+Run the thing:
+```bash
+$ docker-compose up
+```
+
+Now visit `<docker host ip>:8000/omgsickbro` to log into the admin thingy.
--- zombiewalrus/settings.py
+++ zombiewalrus/settings.py
@@ -32,6 +32,35 @@ else:
         }
     }
 
+
+if 'MEMCACHEDCLOUD_SERVERS' in os.environ:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django_bmemcached.memcached.BMemcached',
+            'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
+            'OPTIONS': {
+                'username': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
+                'password': os.environ.get('MEMCACHEDCLOUD_PASSWORD'),
+            },
+            'TIMEOUT': 0,  # Never!!!
+        },
+    }
+elif 'MEMCACHED_SERVERS' in os.environ:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django_bmemcached.memcached.BMemcached',
+            'LOCATION': os.environ.get('MEMCACHED_SERVERS').split(','),
+            'TIMEOUT': 0,  # Never!!!
+        },
+    }
+else:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        },
+    }
+
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -68,7 +97,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = PROJECT_ROOT + '/../media/'
+STATIC_ROOT = PROJECT_ROOT + '/../static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
